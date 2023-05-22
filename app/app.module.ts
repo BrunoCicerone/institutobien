1c8207ed22cd72e4e1d460608c1f5757c00467c9
BrunoCicerone <128143474+BrunoCicerone@users.noreply.github.com>
2023-05-22 14:14:29 +0200

    añadiendo HTTP con una api web

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,6 +8,9 @@ import { PieDePaginaComponent } from './pie-de-pagina/pie-de-pagina.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { BuscadorComponent } from './buscador/buscador.component';
 import { MessagesComponent } from './messages/messages.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,17 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+  
+
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+//Esto lo tenemos que quitar cuando llamaemos al servidor real
+//porque se pone en medio de la llamada y el servidor
+HttpClientInMemoryWebApiModule.forRoot(
+  InMemoryDataService, { dataEncapsulation: false }
+)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app/usuarios.service.ts
+++ app/usuarios.service.ts
@@ -3,15 +3,28 @@ import { Usuario } from './usuarios';
 import { ContenidoUSUARIOS } from './mock-datos';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  private usuariosUrl = 'api/usuarios';  // URL to web api (mas adelante HTTP)
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   getUsuarios(): Observable<Usuario[]> {
-    const listaObservabledeUsuarios = of(ContenidoUSUARIOS);
-    return listaObservabledeUsuarios;
+
+    //cambiamos la llamada de la lista de usuaruios local por llamada HTTP
+    //const listaObservabledeUsuarios = of(ContenidoUSUARIOS);
+    //llamada a enviar el mensaje
+    this.messageService.add('Servicio de usuarios: Usuarios recuperados')
+    //return listaObservabledeUsuarios;
+    return this.http.get<Usuario[]>(this.usuariosUrl)
+
   }
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 }
